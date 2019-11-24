11bcb63cdbe2ca99c1d82cd73c78fd69894d9269
ripclawffb <ripclaw_ffb@hotmail.com>
2019-11-24 07:14:32 -0800

    Upgrade to 2019.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'p4'
-$version = 'r19.1'
+$version = 'r19.2'
 $baseurl = "https://cdist2.perforce.com/perforce/$version"
 $url32 = "$baseurl/bin.ntx86/helix-p4-x86.exe"
 $url64 = "$baseurl/bin.ntx64/helix-p4-x64.exe"
