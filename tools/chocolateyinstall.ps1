9660423e272c31fa88a372dc42a8857a7b3db9c4
Asif Shaikh <ripclaw_ffb@hotmail.com>
2021-05-14 15:16:55 -0700

    Upgrade to 2021.1 (r21.1)

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'p4'
-$version = 'r20.2'
+$version = 'r21.1'
 $baseurl = "https://cdist2.perforce.com/perforce/$version"
 $url32 = "$baseurl/bin.ntx86/helix-p4-x86.exe"
 $url64 = "$baseurl/bin.ntx64/helix-p4-x64.exe"
