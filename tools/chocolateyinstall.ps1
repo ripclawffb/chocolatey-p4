92c18aa2243d5dc6c1f27d5ec76df484ce30cb7f
Asif Shaikh <ripclaw_ffb@hotmail.com>
2024-09-29 12:36:05 -0700

    Upgrade to 2024.1.2655224

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $baseurl = "https://filehost.perforce.com/perforce/$version"
 $url64 = "$baseurl/bin.ntx64/helix-p4-x64.exe"
 
 # Get latest value with ((Invoke-WebRequest "$baseurl/bin.ntx64/SHA256SUMS" -UseBasicParsing).RawContent.ToString().Split() | Select-String -Pattern 'helix-p4-x64.exe' -SimpleMatch -Context 1,0 ).ToString().Trim().Split()[0]
-$checksum64 = '04a5d824dae55a06038bd63308628b7372dd759eb56f473bf53717e8ed3d1db9'
+$checksum64 = 'bf8405ae9468ec1c56f47b0cc55bfb6792628b735ee1641e5a6a509d0952dec6'
 
 $packageArgs = @{
   packageName    = $packageName
